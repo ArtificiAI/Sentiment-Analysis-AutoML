71dc7005ab7981ee3f2b3d32e2a0a043c27a3e40
guillaume-chevalier <guillaume.chevalier@neuraxio.com>
2019-03-28 15:29:28 -0400

    Started to code core

---
+++ sentiment_analysis_auto_ml/sentiment_classifier.py
@@ -0,0 +1,2 @@
+class SentimentClassifier:
+    pass
---
+++ testing/test_sentiment_classifier.py
@@ -0,0 +1,6 @@
+from sentiment_analysis_auto_ml.sentiment_classifier import SentimentClassifier
+
+
+def test_sentiment_classifier():
+    sc = SentimentClassifier()
+    assert True
